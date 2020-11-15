3d8f3aa653ab8f708d9e32ca0095d71b229fe6f2
YBA-IBM <47538942+YBA-IBM@users.noreply.github.com>
2020-11-15 01:30:29 -0500

    Fix to test script
    Signed-off-by: YBA-IBM <yoel.ben-avraham@ibm.com>

--- test.sh
+++ test.sh
@@ -9,12 +9,6 @@ export ANSI_RESET="\e[0m"
 echo -e "\n $ANSI_YELLOW *** FUNCTIONAL TEST(S) *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW Test memcached run per description: $ANSI_RESET"
-docker run --rm -d --name elasticsearch --net somenetwork -p 9400:9400 -p 9500:9500 -e "discovery.type=single-node" quay.io/ibmz/elasticsearch:7.9.1
-docker stop elasticsearch
-docker run --rm -d --name elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" quay.io/ibmz/elasticsearch:7.9.1
-docker stop elasticsearch
-docker network rm somenetwork
-
 docker run --rm --name my-memcache -d quay.io/ibmz/memcached:1.6.8
 docker stop my-memcache
 docker run --rm --name my-memcache -d quay.io/ibmz/memcached:1.6.8 memcached -m 64
